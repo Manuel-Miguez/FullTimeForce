69545be87a8b174da6b6b40d7f04f13eadd8687a
Manuel Miguez <manuelmiguezg@hotmail.com>
2023-03-10 08:55:09 -0400

    adding 404 component, implementing Sorting logic , finishing table component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ModalComponent } from './modal/modal.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [ModalComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent],
+  declarations: [ModalComponent, InputComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [ModalComponent, InputComponent],
 })
 export class SharedModule {}
--- frontend/src/app/table/feature/table/table.component.ts
+++ frontend/src/app/table/feature/table/table.component.ts
@@ -1,8 +1,9 @@
 import { formatDate } from '@angular/common';
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, firstValueFrom, map, catchError } from 'rxjs';
 import { TableService } from '../../data-access/table.service';
 import { CommitHistory } from '../../utils/commit.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-table',
@@ -10,9 +11,13 @@ import { CommitHistory } from '../../utils/commit.interface';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit, OnDestroy {
-  dropDownList = 0;
-  subscriptions$: Subscription[] = [];
-  commits: CommitHistory[] = [];
+  [x: string]: any;
+  public subscriptions$: Subscription[] = [];
+  public commits: CommitHistory[] = [];
+  public isDesc: boolean = false;
+  public nameText!: string;
+  public repoText!: string;
+  public currentColumn!: string;
   modal = {
     title: '',
     showModal: false,
@@ -21,12 +26,35 @@ export class TableComponent implements OnInit, OnDestroy {
     dataCSS: '',
   };
   constructor(@Inject(TableService) private readonly service: TableService) {}
+
   ngOnInit(): void {
-    this.subscriptions$.push(
+    this.nameText = 'tarkah';
+    this.repoText = 'karaoke-rs';
+    this.requestCommitHistory();
+  }
+
+  private async requestCommitHistory() {
+    const result = await firstValueFrom(
       this.service
-        .getRepositoryCommitHistory('tarkah', 'karaoke-rs')
-        .subscribe((response) => (this.commits = response))
+        .getRepositoryCommitHistory(this.nameText, this.repoText)
+        .pipe(
+          map((val) => val),
+          catchError(async (err) => {
+            let message = 'Something went wrong!';
+            if (err.message) message = err.message;
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: message,
+            });
+          })
+        )
     );
+    if (result) this.commits = result;
+  }
+
+  search() {
+    this.requestCommitHistory();
   }
 
   formatDate(date: string) {
@@ -44,6 +72,25 @@ export class TableComponent implements OnInit, OnDestroy {
     this.modal.data = JSON.stringify(showToModal, null, 4);
     this.modal.showModal = true;
     this.modal.title = `Commit: ${commit.sha}`;
+  }
+
+  sort(property: string) {
+    this.isDesc = !this.isDesc;
+    this.currentColumn = property;
+    const direction = this.isDesc ? 1 : -1;
+    this.commits = [...this.commits].sort((a, b) => {
+      const firstValue = this.getValueFromCommit(a, property);
+      const secondValue = this.getValueFromCommit(b, property);
+      if (firstValue.localeCompare(secondValue)) return -1 * direction;
+      else if (secondValue.localeCompare(firstValue)) return 1 * direction;
+      return -1;
+    });
+  }
+
+  private getValueFromCommit(commit: CommitHistory, property: string) {
+    return property === 'name'
+      ? commit.commit.author.name
+      : this.formatDate(commit.commit.committer.date);
   }
 
   ngOnDestroy(): void {
